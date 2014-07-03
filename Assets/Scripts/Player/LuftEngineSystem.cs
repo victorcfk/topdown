fe8729d3846e85dbd5f7c1f24a1c05ded45f53f5
Victor <cfk.infinite@gmail.com>
2014-07-04 01:17:21 +0800

    Commented out luftengine system

--- Assets/Scripts/Player/LuftEngineSystem.cs
+++ Assets/Scripts/Player/LuftEngineSystem.cs
@@ -3,94 +3,97 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class LuftEngineSystem : MonoBehaviour {
-	
-	//[HideInInspector]
+
+    int i =0;
+
+    //[HideInInspector]
     public GameObject controlObj;
     public List<EngineBasic> engines = new List<EngineBasic>();
-
-	public float basicAcceleration = 1.0f;
-	public float basicMaxSpeed = 10.0f;
-	public float basicMaxMagnitudeDelta = 1.0f;
-	
-	protected float resultantAcceleration;
-	protected float resultantMaxSpeed;
-	protected float resultantMaxRadiansDelta;
-	
-	public float horizontalMoveVal;
-	public float verticalMoveVal;
-	
-	// Use this for initialization
-	void Start () {
-		
-		horizontalMoveVal = 0;
-		verticalMoveVal = 0;
-		
-		getAllEngineModifiers();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		
-		getAllEngineModifiers();
-
-		turn(horizontalMoveVal);
-		accelerate(verticalMoveVal);
-		clampMovement();
-
-	}
-	
-	/// <summary>
-	/// Based on fed input, move the gameobject left or right
-	/// </summary>
-	void turn(float input)
-	{   
-		controlObj.transform.Rotate (new Vector3(0,1,0)//controlObj.transform.up
-		                             , -input * resultantMaxRadiansDelta);
-		//Quaternion.
-			//rigidbody.velocity += rightDir * horizontalMoveVal * resultantAcceleration;
-	}
-	
-	/// <summary>
-	/// Based on fed input, move the gameobject up or down
-	/// </summary>
-	void accelerate(float input)
-	{
+    
+    public float basicAcceleration = 1.0f;
+    public float basicMaxSpeed = 10.0f;
+    public float basicMaxMagnitudeDelta = 1.0f;
+    
+    protected float resultantAcceleration;
+    protected float resultantMaxSpeed;
+    protected float resultantMaxRadiansDelta;
+    
+    public float horizontalMoveVal;
+    public float verticalMoveVal;
+    
+    // Use this for initialization
+    void Start () {
+        
+        horizontalMoveVal = 0;
+        verticalMoveVal = 0;
+        
+        getAllEngineModifiers();
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate () {
+        
+        getAllEngineModifiers();
+        
+        turn(horizontalMoveVal);
+        accelerate(verticalMoveVal);
+        clampMovement();
+        
+    }
+    
+    /// <summary>
+    /// Based on fed input, move the gameobject left or right
+    /// </summary>
+    void turn(float input)
+    {   
+        controlObj.transform.Rotate (new Vector3(0,1,0)//controlObj.transform.up
+                                     , -input * resultantMaxRadiansDelta);
+        //Quaternion.
+        //rigidbody.velocity += rightDir * horizontalMoveVal * resultantAcceleration;
+    }
+    
+    /// <summary>
+    /// Based on fed input, move the gameobject up or down
+    /// </summary>
+    void accelerate(float input)
+    {
         //if(input!=0){
         //    foreach (EngineBasic e in engines)
         //        e.activationPS.Play();
         //}
-
+        
         controlObj.rigidbody.velocity += controlObj.transform.forward * input * resultantAcceleration;
-	}
-	
-	/// <summary>
-	/// Control the maximum speed of movement
-	/// </summary>
-	void clampMovement()
-	{
+    }
+    
+    /// <summary>
+    /// Control the maximum speed of movement
+    /// </summary>
+    void clampMovement()
+    {
         controlObj.rigidbody.velocity = Vector3.ClampMagnitude(controlObj.rigidbody.velocity, resultantMaxSpeed);
-	}
-	
-	void getAllEngineModifiers()
-	{
+    }
+    
+    void getAllEngineModifiers()
+    {
+        
         float tempAcc=0;
         float tempMaxSpd = 0;
         float tempMaxTurn = 0;
-
-		foreach (EngineBasic engine in engines)
-		{
+        
+        foreach (EngineBasic engine in engines)
+        {
             tempAcc += engine.moveAccAdd;
             tempMaxSpd += engine.moveSpeedMaxAdd;
-
+            
             tempMaxTurn += engine.turnSpeedAdd;
-		}
+        }
         resultantAcceleration       = basicAcceleration + tempAcc;
         resultantMaxSpeed           = basicMaxSpeed + tempMaxSpd;
-
+        
         resultantMaxRadiansDelta  = basicMaxMagnitudeDelta + tempMaxTurn;
-
-	}
-	
-	
+        
+    }
+    
+    
 }
 
