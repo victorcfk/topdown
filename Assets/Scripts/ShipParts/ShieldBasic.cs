ae39a11ca3852753b062ce516ef5c4e95bf4f21f
Victor <cfk.infinite@gmail.com>
2014-07-26 21:56:52 +0800

    Shields have a hard damage reduction functionality as well.

--- Assets/Scripts/ShipParts/ShieldBasic.cs
+++ Assets/Scripts/ShipParts/ShieldBasic.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class ShieldBasic : DamageReceiver
 {
+    public float dmgHardReduction = 1;
     public float dmgModifier = 0.5f;
     public float healthBoost = 5;
 
     public override void ApplyDamage(float Damage)
     {
-        ParentReceiver.ApplyDamage(Damage * dmgModifier);
+        float temp = (Damage - dmgHardReduction);
+
+        if(temp > 0)
+            ParentReceiver.ApplyDamage( temp * dmgModifier);
     }
 
 }
